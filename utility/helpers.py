df9659d218ad45e20f9256d7e6bb08d29e06f19d
JeffZ <jzhuo@unc.edu>
2022-04-09 02:16:37 -0400

    update helper object

--- utility/helpers.py
+++ utility/helpers.py
@@ -5,11 +5,16 @@ class todo:
     time: str
     location: str
     number: int
+    tag: str
+    participants: list[str]
+    
 
-    def __init__(self, id: int, title: str, description: str, time: str, location: str, number: int):
+    def __init__(self, id: int, title: str, description: str, time: str, location: str, number: int, tag: str, participants: list[str] = []):
         self.id = id
         self.title = title
         self.description = description
         self.time = time
         self.location = location
         self.number = number
+        self.tag = tag
+        self.participants = participants
