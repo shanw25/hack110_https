7d74de7f48827a992719a45c9b241395a88ce2ee
JeffZ <jzhuo@unc.edu>
2022-04-08 22:32:13 -0400

    update firebase name

--- firebase/firebase_client.py
+++ firebase/firebase_client.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-def get_firebase_admin():
+def get_db():
     # Use the application default credentials
     cred = credentials.Certificate("../serviceAccountKey.json")
     firebase_admin.initialize_app(cred)
